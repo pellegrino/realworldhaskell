de65c6b6c207d6ce76d6935d1ea5312bcc263d58
Vitor Pellegrino <vitorp@gmail.com>
2015-11-09 16:01:20 +0100

    Adds code for chapter 05.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Learning real world haskell
 
-Solutions to http://book.realworldhaskell.org/ exercises. 
+Solutions to http://book.realworldhaskell.org/ exercises. =
--- ch05/SimpleJSON.hs
+++ ch05/SimpleJSON.hs
@@ -7,3 +7,23 @@ data JValue = JString String
             | JObject [(String, JValue)]
             | JArray [JValue]
             deriving (Eq, Ord, Show)
+
+
+getString :: JValue -> Maybe String
+getString (JString s) = Just s
+getString _ = Nothing
+
+getInt :: JValue -> Maybe Int
+getInt (JNumber n) = Just (truncate n)
+getInt _           = Nothing
+
+getObject :: JValue -> Maybe [(String, JValue)]
+getObject (JObject o) = Just o 
+getObject  _        = Nothing
+
+getArray :: JValue -> Maybe [JValue]
+getArray (JArray a) = Just a 
+getArray _          = Nothing 
+
+isNull :: JValue -> Bool
+isNull v = v == JNull
